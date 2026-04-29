8d9aaadaea07e830f2931b8c0e0620b267430e61
Bálint Fónad <fonadbalint04@gmail.com>
2026-04-29 14:14:26 +0200

    Implement battery pickup with health restore and UI feedback

---
+++ Assets/Scripts/BatteryManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryManager : MonoBehaviour
+{
+    GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player")) return;
+        if (gameManager.health == 5)
+        {
+            gameManager.StartCoroutine(gameManager.ShowHelpText());
+            return;
+        }
+
+        gameManager.PickUpBattery();
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,17 +8,55 @@ public class GameManager : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] GameObject enemyPrefab;
     [SerializeField] GameObject coinPrefab;
+    [SerializeField] GameObject batteryPrefab;
 
     [SerializeField] TextMeshProUGUI scoreText;
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] TextMeshProUGUI gameOverText;
     [SerializeField] TextMeshProUGUI healthText;
+    [SerializeField] TextMeshProUGUI helpText;
 
     int coinCount = 0;
     int timer = 60;
     public int health = 5;
+    private int maxHealth = 5;
 
     public bool isGameOver = false;
+
+    public IEnumerator ShowHelpText()
+    {
+        int helpTextTimer = 8; // Ennyi ideig fog megjelenni a help szöveg
+        helpText.gameObject.SetActive(true);
+
+        while (helpTextTimer > 0)
+        {
+            yield return new WaitForSeconds(1.0f); // Biztosítja, hogy 1 mp-et várjon mielõtt kivon 1-et a jelenlegi értékbõl
+            helpTextTimer--;
+        }
+
+        helpText.gameObject.SetActive(false);
+    }
+
+    public void PickUpBattery()
+    {
+        health = maxHealth;
+
+        healthText.SetText("Életerõ: " + health);
+
+        SpawnNewBattery();
+    }
+
+    void SpawnNewBattery()
+    {
+        float randomX = Random.Range(-25.0f, 25.0f);
+        float randomZ = Random.Range(-14.0f, 14.0f);
+
+        Vector3 offset = new Vector3(randomX, 0, randomZ);
+
+        Instantiate(batteryPrefab,
+            coinPrefab.transform.position + offset,
+            batteryPrefab.transform.rotation);
+    }
 
     public void TakeDamage()
     {
@@ -75,6 +113,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        helpText.gameObject.SetActive(false);
         gameOverText.gameObject.SetActive(false);
         isGameOver = false;
 
